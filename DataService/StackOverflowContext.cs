21fa4247890ce4e00158c8f412e91dbdf812b2f1
siinch <simon.ingemanns@protonmail.com>
2019-11-01 12:33:01 +0100

    simplewebservice

--- DataService/StackOverflowContext.cs
+++ DataService/StackOverflowContext.cs
@@ -4,7 +4,7 @@ namespace rawdata_portfolioproject_2
 {
     public class StackOverflowContext : DbContext
     {
-
+        // optional: make functions for making mapping easier... look at livecode6 for inspiration
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // map relational model to object model like so:

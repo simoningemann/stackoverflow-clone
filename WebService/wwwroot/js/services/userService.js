74c3877702b4fc0a15450cb64998733527b649ad
siinch <simon.ingemanns@protonmail.com>
2019-12-05 14:15:38 +0100

    jsuserservice

---
+++ WebService/wwwroot/js/services/userService.js
@@ -0,0 +1,27 @@
+define([], function() {
+    
+    var getUser = async function (callback, userId) {
+        var response = await fetch("api/users/" + userId);
+        var data = await response.json();
+        callback(data);
+    };
+    
+    var getUsers = async function (callback, userIds) {
+        var url = createGetUsersUrl(userIds);
+        var response = await fetch(url);
+        var data = await response.json();
+        callback(data);
+    };
+    
+    function createGetUsersUrl (userIds) {
+        var url = "api/users?";
+        for(var userId of userIds)
+            url = url + "userIds" + userId + "&";
+        url = url.slice(0, url.length-1);
+        return url;
+    }
+    
+    return {
+        getUser
+    };
+});

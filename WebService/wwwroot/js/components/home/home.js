ea372e6fa78f0fc405b1f82e6b37dee9ad5de866
siinch <simon.ingemanns@protonmail.com>
2019-12-06 19:50:06 +0100

    simplesearchinterface

--- WebService/wwwroot/js/components/home/home.js
+++ WebService/wwwroot/js/components/home/home.js
@@ -1,9 +1,21 @@
-define(["knockout", "postmanager"], function(ko, pm) {
+define(["knockout", "postmanager", "questionService"], function(ko, pm, qs) {
     return function () {
         console.log("hello from home");
-
+        var input = ko.observable("");
+        var results = ko.observable({});
+        
+        var search = async function () {
+            var keywords = input().split(" ");
+            await qs.searchQuestions(function(data) {
+                results(data);
+            }, 1, 10, keywords);
+            console.log(results());
+        };
+        
         return {
-
+            input,
+            search,
+            results
         }
     }
 });
